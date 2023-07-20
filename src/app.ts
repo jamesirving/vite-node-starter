ff84e886ddffed954c0193984dcb32b29734cf7e
David Lee <dav@davlee.com>
2023-07-20 13:26:44 +1000

    first post

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,82 @@
-import {program} from 'commander';
-import figlet from 'figlet';
-import * as fs from 'fs';
-import inquirer from 'inquirer';
-import * as dotenv from 'dotenv';
-import * as log from './console-logger';
+// import {program} from 'commander'
+// import * as fs from 'fs'
+import * as dotenv from 'dotenv'
+import chalk from 'chalk'
+import * as blessed from "blessed"
+import * as log from './console-logger'
 
-dotenv.config({path: '.env'});
+dotenv.config({path: '.env'})
+log.info(chalk.blue('Hello world'))
 
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-
-program
-    .version(packageJson.version)
-    .option('-say, --say <string>', 'Say something')
-    .parse(process.argv);
-
-const opts = program.opts();
-
-async function main() {
-    console.log(figlet.textSync(process.env.APP_TITLE ?? 'My App'));
-
-    let say = opts.say;
-    if (say === undefined) {
-        log.warn('No `say` option provided');
-        const answers = await inquirer.prompt([
-            {
-                type: 'input',
-                name: 'say',
-                message: 'Say:',
-            },
-        ]);
-        say = answers.say;
-    }
-    log.info(`👋 ${say}`);
+// y, x
+function generateGrid(): String[][] {
+  let grid : String[][]  = Array(height).fill(' ').map(() => 
+    Array(width).fill(' ').map(() => randomAsciiChar()
+  ))
+  grid.toString = () => gridToString(grid)
+  return grid
 }
 
-main().catch(console.error);
+function gridToString(grid: String[][]): string {
+  return grid.map(row => row.join('')).join('\n')
+}
+
+const characters = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.?!@#$%^&*(:;'"\\|)}]{[<>/-_=+\`~`.split('');
+const width = 120
+const height = 80
+
+function randomAsciiChar(): String {
+  let i = (Math.random() * characters.length) | 0
+  return characters[i]
+}
+
+let grid = generateGrid()
+
+//
+//
+//
+
+const screen = blessed.screen({
+  smartCSR: true
+})
+
+screen.title = 'best title'
+
+screen.key(["q", "C-c"], () => {
+  screen.destroy()
+  process.exit(0) 
+})
+
+const box = blessed.box({
+  top: 'center',
+  left: 'center',
+  width: 120+2,
+  height: 80+2,
+  content: grid.toString(),
+  tags: true,
+  mouse: false,
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'white',
+    bg: 'black',
+    border: {
+      fg: '#f0f0f0'
+    },
+    hover: {
+      bg: 'green'
+    }
+  }
+})
+
+
+screen.append(box)
+screen.render()
+
+//
+//
+//
+
+
+
